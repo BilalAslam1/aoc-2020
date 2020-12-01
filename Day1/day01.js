5e137bf8f398992eb9ce5495f92da9167adc2b34
Bilal Aslam <bilal.aslam.sheikh@gmail.com>
2020-12-01 18:39:40 -0500

    restructured reading file to read once and pass into both parts of challenge

--- Day1/day01.js
+++ Day1/day01.js
@@ -1,23 +1,25 @@
-var fs = require("fs");
+const fs = require("fs");
+const num_array = fs.readFileSync("./day01-input.txt", "utf-8").split("\n").map(Number);
 
-const partOne = () => {
-    fs.readFile("./day01-input.txt", function (num_array) {
-        var num_array = fs.readFileSync("./day01-input.txt", "utf-8").split("\n").map(Number);
-        dataSet = new Set();
+const partOne = (num_array) => {
 
-        for (i in num_array) {
+    dataSet = new Set();
+    for (i in num_array) {
+        if (dataSet.has(2020 - num_array[i])) {
+            console.log("The two numbers that add up to 2020 are: ", num_array[i], ",", 2020 - num_array[i], "\nThere sum is: ", num_array[i] * (2020 - num_array[i]));
 
-            if (dataSet.has(2020 - num_array[i])) {
-                console.log("The two numbers that add up to 2020 are: ", num_array[i], ",", 2020 - num_array[i], "\nThere sum is: ", num_array[i] * (2020 - num_array[i]));
-
-            }
-            else
-                dataSet.add(num_array[i]);
         }
-    });
+        else
+            dataSet.add(num_array[i]);
+    }
 }
 
-partOne();
+const partOne = (num_array) => {
+
+}
 
 // This soultion has time complexity of O(N) but a space complexity of O(N) in worst case scenario as well. 
 // The space complixity can be reduced to O(1) at expense of increase time complexity to O(N^2)
+partOne(num_array);
+
+partTwo(num_array);
