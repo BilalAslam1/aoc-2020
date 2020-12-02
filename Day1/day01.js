b2b10f4c6280039468ff6a661c9468ec61f8a77d
Bilal Aslam <bilal.aslam.sheikh@gmail.com>
2020-12-01 19:30:34 -0500

    finished part two of day 1

--- Day1/day01.js
+++ Day1/day01.js
@@ -5,21 +5,34 @@ const partOne = (num_array) => {
 
     dataSet = new Set();
     for (i in num_array) {
-        if (dataSet.has(2020 - num_array[i])) {
+        if (dataSet.has(2020 - num_array[i]))
             console.log("The two numbers that add up to 2020 are: ", num_array[i], ",", 2020 - num_array[i], "\nThere sum is: ", num_array[i] * (2020 - num_array[i]));
-
-        }
         else
             dataSet.add(num_array[i]);
     }
 }
 
-const partOne = (num_array) => {
-
+const partTwo = (num_array) => {
+    dataSet = new Set();
+    for (var x = 0; x < num_array.length - 1; x++) {
+        for (var y = x + 1; y < num_array.length - 1; y++) {
+            if (dataSet.has(2020 - num_array[x] - num_array[y])) {
+                return console.log("Three numbers that add up to 2020 are: ", num_array[x], ",", num_array[y], ",", 2020 - num_array[x] - num_array[y]
+                    , "\nTheir sum is: ", num_array[x] * num_array[y] * (2020 - num_array[x] - num_array[y]))
+            }
+            else {
+                dataSet.add(num_array[x]);
+                dataSet.add(num_array[y]);
+            }
+        }
+    }
 }
 
+
 // This soultion has time complexity of O(N) but a space complexity of O(N) in worst case scenario as well. 
-// The space complixity can be reduced to O(1) at expense of increase time complexity to O(N^2)
+// The space complixity can be reduced to O(1) using brute force at expense of increase time complexity to O(N^2)
 partOne(num_array);
 
+// This solution loops through O(N^2) times using same approach of hash tables to reduce time complexity but increase space complexity to O(N). 
+// The space complixity can be reduced to O(1) at expense of increase time complexity to O(N^3) using 3 for loops 
 partTwo(num_array);
